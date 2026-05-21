aa6c2e9434aeb58159184aa1f3b637193517f114
ymeng <y.meng@tudelft.nl>
2026-05-21 20:32:20 +0200

    refactor: remove myintegrator, using default bfi (SymbolicBFI).

--- src/mymodule.cpp
+++ src/mymodule.cpp
@@ -2,23 +2,12 @@
 #include <python_comp.hpp>
 #include <pybind11/stl.h>
 
-#include "myintegrator.hpp"
 #include "myassembling.hpp"
 
 
 PYBIND11_MODULE(myassembling, m)
 {
   cout << "Loading myassembling library" << endl;
-
-  py::class_<ngfem::MyLaplaceIntegrator, shared_ptr<ngfem::MyLaplaceIntegrator>, ngfem::BilinearFormIntegrator>
-    (m, "MyLaplace")
-    .def(py::init<shared_ptr<ngfem::CoefficientFunction>>())
-    ;
-  
-  py::class_<ngfem::MySourceIntegrator, shared_ptr<ngfem::MySourceIntegrator>, ngfem::LinearFormIntegrator>
-    (m, "MySource")
-    .def(py::init<shared_ptr<ngfem::CoefficientFunction>>())
-    ;
 
   py::class_<ngcomp::LocalSupportMatrix, shared_ptr<ngcomp::LocalSupportMatrix>>
     (m, "LocalSupportMatrix")
